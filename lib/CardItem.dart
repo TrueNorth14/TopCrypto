82e2065bf0cd5e721eaa67645a5c00e40e26f9ff
TrueNorth14 <vishnu514.nair@gmail.com>
2019-08-13 11:42:45 -0400

    Made getCard function into a seperate class called CardItem s Please enter the commit message for your changes. Lines starting

---
+++ lib/CardItem.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+import 'utilities.dart';
+import 'package:flutter_sparkline/flutter_sparkline.dart';
+
+class CardItem extends StatelessWidget{
+  final Coin c;
+
+  const CardItem({Key key, this.c}) : super(key: key);
+
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Padding(
+    padding: EdgeInsets.all(5),
+    child: GestureDetector(
+      onTapDown: (TapDownDetails) {
+        print("tapped");
+      },
+      onTap: () {
+        //print("$coinData");
+        print("pressed $c");
+      },
+      child: SizedBox(
+        height: 230,
+        child: Card(
+          elevation: 20,
+          borderOnForeground: true,
+          //borderRadius: BorderRadius.circular(8),
+
+          //margin: EdgeInsets.all(8),
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.fromLTRB(15, 15, 0, 5),
+                child: Row(
+                  children: <Widget>[
+                    Text(
+                      c.name,
+                      textScaleFactor: 1.5,
+                      style: TextStyle(fontWeight: FontWeight.w400),
+                    ),
+                    Spacer(),
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(0, 0, 15, 0),
+                      child: Text(
+                        "\$" +
+                            ((c.price > 1)
+                                ? c.price
+                                    .toStringAsFixed(2)
+                                    .replaceAllMapped(reg, mathFunc)
+                                : c.price.toStringAsFixed(4)),
+                        textScaleFactor: 1.5,
+                        style: TextStyle(fontWeight: FontWeight.w400),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Row(
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(15, 0, 8, 0),
+                    child: CircleAvatar(
+                      backgroundColor: Color(getColorFromHex(c.color)),
+                      radius: 8,
+                    ),
+                  ),
+                  Text(
+                    c.symbol,
+                    style: TextStyle(fontWeight: FontWeight.w300),
+                  ),
+                  Spacer(),
+                  (c.change > 0)
+                      ? Icon(Icons.arrow_drop_up, color: Colors.green)
+                      : Icon(Icons.arrow_drop_down, color: Colors.red),
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(0, 0, 15, 0),
+                    child: Text(
+                      c.change.toString() + "%",
+                      style: TextStyle(
+                          fontWeight: FontWeight.w300,
+                          color: (c.change > 0) ? Colors.green : Colors.red),
+                    ),
+                  )
+                ],
+              ),
+              Spacer(),
+              Sparkline(
+                fallbackHeight: 130,
+                data: c.history,
+                lineColor: (c.change > 0) ? Colors.green : Colors.red,
+                fillMode: FillMode.below,
+                fillGradient: (c.change > 0)
+                    ? LinearGradient(
+                        colors: [Colors.green[100], Colors.white],
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter)
+                    : LinearGradient(
+                        colors: [Colors.red[100], Colors.white],
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter),
+              ),
+            ],
+          ),
+        ),
+      ),
+    ),
+  );
+  }
+}
--- lib/app.dart
+++ lib/app.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'main.dart';
 import 'utilities.dart';
 import 'package:flutter_sparkline/flutter_sparkline.dart';
+import 'CardItem.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 
@@ -92,7 +93,7 @@ class MyAppState extends State<MyApp> {
               itemCount: snapshot.data.length,
               scrollDirection: Axis.vertical,
               itemBuilder: (BuildContext context, int index) {
-                return getCard(snapshot.data[index]);
+                return CardItem(c: snapshot.data[index]);
               },
             ),
           );
@@ -152,112 +153,4 @@ class MyAppState extends State<MyApp> {
       ),
     );
   }
-}
-
-Widget getCard(Coin c) {
-  //String _shortHand = "unknown";
-
-  return Padding(
-    padding: EdgeInsets.all(5),
-    child: GestureDetector(
-      onTapDown: (TapDownDetails) {
-        print("tapped");
-      },
-      onTap: () {
-        //print("$coinData");
-        print("pressed $c");
-      },
-      child: SizedBox(
-        height: 230,
-        child: AnimatedContainer(
-          duration: Duration(seconds: 1),
-          curve: Curves.elasticInOut,
-          //height: MyAppState.dimensions.height,
-          //width: MyAppState.dimensions.width,
-          child: Card(
-            elevation: 20,
-            borderOnForeground: true,
-            //borderRadius: BorderRadius.circular(8),
-
-            //margin: EdgeInsets.all(8),
-            child: Column(
-              mainAxisSize: MainAxisSize.max,
-              children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.fromLTRB(15, 15, 0, 5),
-                  child: Row(
-                    children: <Widget>[
-                      Text(
-                        c.name,
-                        textScaleFactor: 1.5,
-                        style: TextStyle(fontWeight: FontWeight.w400),
-                      ),
-                      Spacer(),
-                      Padding(
-                        padding: EdgeInsets.fromLTRB(0, 0, 15, 0),
-                        child: Text(
-                          "\$" +
-                              ((c.price > 1)
-                                  ? c.price
-                                      .toStringAsFixed(2)
-                                      .replaceAllMapped(reg, mathFunc)
-                                  : c.price.toStringAsFixed(4)),
-                          textScaleFactor: 1.5,
-                          style: TextStyle(fontWeight: FontWeight.w400),
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-                Row(
-                  children: <Widget>[
-                    Padding(
-                      padding: EdgeInsets.fromLTRB(15, 0, 8, 0),
-                      child: CircleAvatar(
-                        backgroundColor: Color(getColorFromHex(c.color)),
-                        radius: 8,
-                      ),
-                    ),
-                    Text(
-                      c.symbol,
-                      style: TextStyle(fontWeight: FontWeight.w300),
-                    ),
-                    Spacer(),
-                    (c.change > 0)
-                        ? Icon(Icons.arrow_drop_up, color: Colors.green)
-                        : Icon(Icons.arrow_drop_down, color: Colors.red),
-                    Padding(
-                      padding: EdgeInsets.fromLTRB(0, 0, 15, 0),
-                      child: Text(
-                        c.change.toString() + "%",
-                        style: TextStyle(
-                            fontWeight: FontWeight.w300,
-                            color: (c.change > 0) ? Colors.green : Colors.red),
-                      ),
-                    )
-                  ],
-                ),
-                Spacer(),
-                Sparkline(
-                  fallbackHeight: 130,
-                  data: c.history,
-                  lineColor: (c.change > 0) ? Colors.green : Colors.red,
-                  fillMode: FillMode.below,
-                  fillGradient: (c.change > 0)
-                      ? LinearGradient(
-                          colors: [Colors.green[100], Colors.white],
-                          begin: Alignment.topCenter,
-                          end: Alignment.bottomCenter)
-                      : LinearGradient(
-                          colors: [Colors.red[100], Colors.white],
-                          begin: Alignment.topCenter,
-                          end: Alignment.bottomCenter),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-    ),
-  );
 }
